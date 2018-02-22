8252f798d9bf74d1d69ed7256157ebc4acea96d5
Sylvain Bourgin <sylvain.bourgin@gmail.com>
2018-02-22 15:01:35 -0800

    Hello world for hibernate prototype

---
+++ Prototypes/Hibernate/src/main/java/Hello.java
@@ -0,0 +1,15 @@
+
+/**
+ * Created by sylvain on 2/22/18.
+ */
+public class Hello {
+
+    /**
+     * Application entry point method.
+     * @param args Arguments
+     */
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
